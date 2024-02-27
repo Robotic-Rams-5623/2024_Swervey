34b28e3bfc7d6cb27cbe6b72e1d166708a187327
Craig <45637739+ziehmerc@users.noreply.github.com>
2024-02-27 14:56:18 -0500

    Update teleopDrive.java

--- src/main/java/frc/robot/commands/teleopDrive.java
+++ src/main/java/frc/robot/commands/teleopDrive.java
@@ -11,53 +11,28 @@ import frc.robot.Constants;
 import frc.robot.subsystems.SwerveDriveBase;
 
 public class teleopDrive extends Command {
-  private SwerveDriveBase s_swerve;
-    private DoubleSupplier translationSup;
-    private DoubleSupplier strafeSup;
-    private DoubleSupplier rotationSup;
-    private BooleanSupplier robotCentricSup;
-
-    private SlewRateLimiter translationLimiter = new SlewRateLimiter(3.0);
-    private SlewRateLimiter strafeLimiter = new SlewRateLimiter(3.0);
-    private SlewRateLimiter rotationLimiter = new SlewRateLimiter(3.0);
+  private final SwerveDriveBase m_drive;
+  private final CommandXboxController m_controller;
   
-  public teleopDrive(
-      SwerveDriveBase s_Swerve,
-      DoubleSupplier translationSup,
-      DoubleSupplier strafeSup,
-      DoubleSupplier rotationSup,
-      BooleanSupplier robotCentricSup) {
-    
-    this.s_swerve = s_Swerve;
-    addRequirements(s_swerve);
-
-    this.translationSup = translationSup;
-    this.strafeSup = strafeSup;
-    this.rotationSup = rotationSup;
-    this.robotCentricSup = robotCentricSup;
+  public teleopDrive(SwerveDriveBase drive, CommandXboxController controller) {
+    m_drive = drive;
+    m_controller = controller;
+    addRequirements(m_drive);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    /* Get Values and Deadband */
-    double translation = translationLimiter.calculate(
-      MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.OperatorConstants.kDriverDeadband)
-    );
+    double desiredTrans[] = MathUtils.inputTransform(-m_controller.getLeftY(), -m_controller.getLeftX());
+    double maxLinear = Constants.Swerve.kMaxDriveMeterPerSec;
 
-    double strafe = strafeLimiter.calculate(
-      MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.OperatorConstants.kDriverDeadband)
-    );
+    desiredTrans[0] *= maxLinear;
+    desiredTrans[1] *= maxLinear;
 
-    double rotation = rotationLimiter.calculate(
-      MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.OperatorConstants.kDriverDeadband)
-    );
+    double desiredRot = -MathUtils.inputTransform(m_controller.getRightX()) * Constants.Swerve.kMaxTurnRadianPerSec;
 
-    s_swerve.drive(
-      new Translation2d(translation, strafe).times(0.5),
-      rotation * 0.5, 
-      !robotCentricSup.getAsBoolean(),
-      true
-    );
+    m_drivetrain.drive(desiredTrans[0], desiredTrans[1], desiredRot, true, true);
+
+    SmartDashboard.putBoolean("DrivingByController", true);
   }
 }
