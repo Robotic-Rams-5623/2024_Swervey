32b4fab70d70c4f28342fc045840d09355ff3f5d
Craig <45637739+ziehmerc@users.noreply.github.com>
2024-02-29 12:40:50 -0500

    Update RobotContainer.java
    Changes for YAGSL

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,24 +13,59 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   /* ROBOT SUBSYSTEM DEFINITIONS */
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),"swerve/neo"));
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  // private final SwerveDriveBase m_drive = new SwerveDriveBase();
 
   /* SET DRIVER CONTROLLER OBJECTS */
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverUSBPort);
-  // private final CommandXboxController m_actionController = 
-  //     new CommandXboxController(OperatorConstants.kActionUSBPort);
+  final CommandXboxController m_driverXbox = new CommandXboxController(OperatorConstants.kDriverUSBPort);
+  // final CommandXboxController m_actionXbox = new CommandXboxController(OperatorConstants.kActionUSBPort);
 
-  
+
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    /* SET DEFAULT COMMANDS */
-    configureDefaults();
+    /* CONFIGURE PRE-DEFINED COMMANDS */
+    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
+                                                                   () -> -MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                                OperatorConstants.kDriverDb_LeftX),
+                                                                   () -> -MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                                OperatorConstants.kDriverDb_LeftY),
+                                                                   () -> -MathUtil.applyDeadband(driverXbox.getRightX(),
+                                                                                                OperatorConstants.kDriverDb_RightX),
+                                                                   driverXbox.getHID()::getYButtonPressed,
+                                                                   driverXbox.getHID()::getAButtonPressed,
+                                                                   driverXbox.getHID()::getXButtonPressed,
+                                                                   driverXbox.getHID()::getBButtonPressed);
+    /* Applies deadbands and inverts controls because joysticks are
+     * back-right positive while the robot controls are front-left positive.
+     * LEFT STICK controls translation
+     * RIGHT STICK controls the desired angle, NOT angular rotation.
+     */
+    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
+        () -> MathUtil.applyDeadband(driverXbox.getLeftY(), OperatorConstants.kDriverDb_LeftY),
+        () -> MathUtil.applyDeadband(driverXbox.getLeftX(), OperatorConstants.kDriverDb_LeftX),
+        () -> driverXbox.getRightX(),
+        () -> driverXbox.getRightY());
+
+    /* Applies deadboand and inverts controls becasue joysticks are
+     * back right positive while robot controls are fonrt-left positive.
+     * LEFT STICK controls translation
+     * RIGHT STICK controls angular velocity of robot
+     */
+    Command driveFieldOrientedAnglularVelocity = drivebase.driveCommand(
+        () -> MathUtil.applyDeadband(driverXbox.getLeftY(), OperatorConstants.kDriverDb_LeftY),
+        () -> MathUtil.applyDeadband(driverXbox.getLeftX(), OperatorConstants.kDriverDb_LeftX),
+        () -> driverXbox.getRightX() * 0.5);
+    // Uncomment the following if you plan on using the code simulation which might not be setup.
+    // Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
+    //     () -> MathUtil.applyDeadband(driverXbox.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND),
+    //     () -> MathUtil.applyDeadband(driverXbox.getLeftX(), OperatorConstants.LEFT_X_DEADBAND),
+    //     () -> driverXbox.getRawAxis(2));
     
-    /* CONFIGURE BUTTON BINDINGS */
-    configureBindings();
+    /* SET DEFAULT COMMANDS & BUTTON BINDINGS */
+    configureDefaults();
+    configureDriveBindings();
+    configureActionBindings();
   }
 
 
@@ -41,7 +76,9 @@ public class RobotContainer {
    */
   private void configureDefaults() {
     /* Drivetrain Default Command */
-    // m_drive.setDefaultCommand(new teleopDrive(m_drive, m_driverController));
+    // If wanting to run in simulation mode (might not be setup), swap which line is commented.
+    //drivebase.setDefaultCommand(!RobotBase.isSimulation() ? driveFieldOrientedDirectAngle : driveFieldOrientedDirectAngleSim);
+    drivebase.setDefaultCommand(driveFieldOrientedDirectAngle);
 
     /* Example Subsystem Default Command */
     // m_exampleSubsystem.setDefaultCommand(new ExampleCommand(m_exampleSubsystem));
@@ -54,11 +91,23 @@ public class RobotContainer {
    * Use this method to define your trigger->command mappings. Triggers can be controller button
    * presses, events that occur, or even limit switches on the robot.
    */
-  private void configureBindings() {
-    /* CONTROLLER COMMAND */
-    // END CONTROLLER COMMAND
+  private void configureDriveBindings() {
+    /* Reset Gyro Angle Manually */
+    driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+    
+    /* Manually Add a Fake Vision Reading??? or lock the wheels into defense mode */
+    driverXbox.x().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
+    // driverXbox.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
 
+    /* DRIVE TO A SPECIFIC POSITION AT AN ANGLE OF ZERO */
+    driverXbox.b().whileTrue(
+        Commands.deferredProxy(() -> drivebase.driveToPose(
+                                   new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
+                              ));
+    
   }
+
+  private void configureActionBindings() {}
 
 
 
@@ -70,6 +119,10 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return drivebase.getAutonomousCommand("New Auto");
+  }
+
+  public void setMotorBrake(boolean brake) {
+    drivebase.setMotorBrake(brake);
   }
 }
